9ed481fbb81eae9ebf55391e5d6806ac0486d721
Shashi <shashi@intandemly.com>
2026-07-24 03:51:55 +0530

    routes and api

--- fuelroute/admin.py
+++ fuelroute/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from .models import FuelStation
 
 
+
 @admin.register(FuelStation)
 class FuelStationAdmin(admin.ModelAdmin):
     list_display = ("name", "city", "state", "retail_price", "latitude", "longitude")
